aa375a2ed32cfee717fd86c4eb791284fc3cc978
asim178 <152957417+asim178@users.noreply.github.com>
2023-12-05 12:44:12 +0500

    Add files via upload

---
+++ continue_break_statement.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+	<?php 
+    for ($i=0; $i <= 10 ; $i++) { 
+    	if ($i == 1)
+    	{
+    		echo "</br> odd number </br> ";
+    		break;
+    	}
+    	
+    	echo "</br>even number</br>";
+    }
+
+
+	?>
+</body>
+</html>
